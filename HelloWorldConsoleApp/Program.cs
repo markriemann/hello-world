a84772a9f8b51c1b055c41a1197599764a26c650
Mark Riemann <mark.riemann@hotmail.com>
2020-11-20 19:51:05 -0600

    Added C# console app Hello World

---
+++ HelloWorldConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+
+namespace HelloWorldConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            for(var i =0; i < 30; i++)
+            {
+                Console.WriteLine(i + 1);
+                Thread.Sleep(1000);
+            }
+
+            Console.WriteLine("Hello World ENDED");
+        }
+    }
+}
